82c23d9afed74f7a7213142121a7da59883a6ebd
apereslavtsev <pereslavtsev.anton@gmail.com>
2022-03-14 23:29:11 +0300

    1. Создать элементарную структуру дерева [#1711]

--- src/main/java/ru/job4j/tree/Tree.java
+++ src/main/java/ru/job4j/tree/Tree.java
@@ -2,6 +2,7 @@ package ru.job4j.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public interface Tree<E> {
@@ -14,6 +15,23 @@ public interface Tree<E> {
 
         public Node(E value) {
             this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (!(o instanceof Node)) {
+                return false;
+            }
+            Node<?> node = (Node<?>) o;
+            return value.equals(node.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
         }
     }
 
