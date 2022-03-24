d6a76d8c420de3f9918186bc7e7ff8b9d8b0befd
apereslavtsev <pereslavtsev.anton@gmail.com>
2022-03-24 21:55:02 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class Matrix {
+    public static int[][] multiple(int size) {
+        int[][] rsl = new int[size][size];
+        for (int row = 1; row <= size; row++) {
+            for (int cell = 1; cell <= size; cell++) {
+                rsl[row - 1][cell - 1] = row * cell;
+            }
+        }
+        return rsl;
+    }
+
+    public static void saveInFile(int[][] matrix, String fileName) {
+
+        StringBuilder writeString = new StringBuilder();
+        for (int[] line :  matrix) {
+            for (int cell : line) {
+                writeString.append(cell + " ");
+            }
+            writeString.append(System.lineSeparator());
+        }
+        try (FileOutputStream out = new FileOutputStream(fileName)) {
+            out.write(writeString.toString().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        saveInFile(multiple(4), "test4");
+    }
+}
