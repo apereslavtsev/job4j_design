e7111d89a36238f84b2adda3821f024103c2120c
apereslavtsev <pereslavtsev.anton@gmail.com>
2022-03-26 14:01:41 +0300

    2. Анализ доступности сервера. [#859 #280383]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -10,40 +10,37 @@ public class Analizy {
         /* file read*/
         List<String> fileData = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
-            fileData = in.lines().collect(Collectors.toList());
+            /* save in file */
+            try (PrintWriter out = new PrintWriter(
+                    new BufferedOutputStream(
+                            new FileOutputStream(target)
+                    ))) {
+                String line;
+                boolean unavailable = false;
+                while ((line = in.readLine()) != null) {
+                    /*new file generate logic*/
+                    if (!unavailable
+                            && (line.startsWith("400")
+                            || line.startsWith("500"))) {
+                        out.append(line.split(" ", 2)[1])
+                                .append(";");
+                        unavailable = true;
+                    } else if (unavailable
+                            && (line.startsWith("200")
+                            || line.startsWith("300"))) {
+                        out.append(line.split(" ", 2)[1])
+                                .append(System.lineSeparator());
+                        unavailable = false;
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Ошибка при записи данных в файл! " + target);
+                e.printStackTrace();
+            }
         } catch (IOException e) {
             System.out.println("Ошибка при чтении данных из файла!");
             e.printStackTrace();
         }
-        /* work logic*/
-        StringBuilder text = new StringBuilder();
-        boolean unavailable = false;
-        for (String line : fileData) {
-            if (!unavailable
-            && (line.startsWith("400")
-                    || line.startsWith("500"))) {
-                text.append(line.split(" ", 2)[1])
-                        .append(";");
-                unavailable = true;
-            } else if (unavailable
-                    && (line.startsWith("200")
-                    || line.startsWith("300"))) {
-                text.append(line.split(" ", 2)[1])
-                        .append(System.lineSeparator());
-                unavailable = false;
-            }
-        }
-        /* save in file */
-        try (PrintWriter out = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-                out.print(text);
-        } catch (IOException e) {
-            System.out.println("Ошибка при записи данных в файл! " + target);
-            e.printStackTrace();
-        }
-
     }
 
     public static void main(String[] args) {
