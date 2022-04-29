c9dde131a40b9bc8fc9611f3522b6ae39ef70620
apereslavtsev <pereslavtsev.anton@gmail.com>
2022-04-29 11:15:26 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -10,7 +10,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-
     public void packFiles(List<File> listSources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (File source : listSources) {
@@ -33,29 +32,24 @@ public class Zip {
             e.printStackTrace();
         }
     }
-
     public static void main(String[] args) throws IOException {
-
-        ArgsName arguments = ArgsName.of(args);
-        if (!Search.argsValid(arguments.get("d"), arguments.get("e"))) {
-            return;
-        }
-
+        ArgsName arguments = validArgs(args);
         Path start = Paths.get(arguments.get("d"));
         Predicate<Path> condition = path -> !path.toFile().getName().endsWith(arguments.get("e"));
         List<File> fileList = Search.search(start, condition).stream()
-                .map(p -> p.toFile())
+                .map(Path::toFile)
                 .collect(Collectors.toList());
-
         Zip zip = new Zip();
         zip.packFiles(fileList,
                 new File(arguments.get("o")));
-
-        /* Zip zip = new Zip();
-        zip.packSingleFile(
-                new File("./pom.xml"),
-                new File("./pom.zip")
-        );*/
-
+    }
+    private static ArgsName validArgs(String[] args) {
+        ArgsName arguments = ArgsName.of(args);
+        Search.argsValid(arguments.get("d"), arguments.get("e"));
+        File target = new File(arguments.get("o"));
+        if (!new File(new File(target.getAbsolutePath()).getParent()).exists()) {
+            throw new IllegalArgumentException("Wrong target catalog!");
+        }
+        return arguments;
     }
 }
