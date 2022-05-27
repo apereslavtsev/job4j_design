1e144ac42ba8da75c1c43541e2af7fcf0b5e86b9
apereslavtsev <pereslavtsev.anton@gmail.com>
2022-05-27 18:52:17 +0300

    3. Бот [#7921 #305859]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -7,30 +7,29 @@ public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
-                boolean closeRequestReceived = false;
+
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        System.out.println(str);
-                        if (str.matches("(.+)msg=(.+)")) {
-                            String msg = str.split("=")[1].split("HTTP")[0].trim();
-                            if ("Hello".equals(msg)) {
-                                out.write("Hello, my dear friend!".getBytes());
-                            } else if ("Exit".equals(msg)) {
-                                out.write("Bye bye!".getBytes());
-                                closeRequestReceived = true;
-                            } else {
-                                out.write("What".getBytes());
-                            }
+                    String clientRequest = in.readLine();
+                    if (clientRequest.matches("(.+)msg=(.+)")) {
+                        String msg = clientRequest.split("=")[1].split("HTTP")[0].trim();
+                        if ("Hello".equals(msg)) {
+                            out.write("Hello, my dear friend!".getBytes());
+                        } else if ("Exit".equals(msg)) {
+                            out.write("Bye bye!".getBytes());
+                            server.close();
+                        } else {
+                            out.write("What".getBytes());
                         }
                     }
-                    out.flush();
-                    if (closeRequestReceived) {
-                        server.close();
+                    System.out.println(clientRequest);
+                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+                        System.out.println(str);
                     }
+                    out.flush();
                 }
             }
         }
