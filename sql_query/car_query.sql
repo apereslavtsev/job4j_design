e0ce4fff4eaf4493ed96c1c8e4c5db1b3ada324e
apereslavtsev <pereslavtsev.anton@gmail.com>
2022-06-21 11:03:42 +0300

    0. Что такое база данных. [#6860]

--- sql_query/car_query.sql
+++ sql_query/car_query.sql
@@ -1,10 +1,3 @@
-/*create table cars (
-    id serial primary key, 
-    carName text, 
-    wheelCount integer,
-    weight double precision
-);*/
-
 insert into cars (carName, wheelCount) values ('GAZ 66', 6);
 select * from cars;
 
