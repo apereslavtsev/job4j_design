769ee29ea6cb5ff410c1382c08c04ec694066fc2
apereslavtsev <pereslavtsev.anton@gmail.com>
2022-06-21 11:07:37 +0300

    0.1. Связи между таблицами [#373615]

---
+++ sql_query/car_references.sql
@@ -0,0 +1,37 @@
+create table colors
+(
+	id serial primary key,
+    colorDescription text,
+    rgbCode text,
+    htmlCode text     
+);
+
+create table pts
+(
+	id serial primary key,
+    seria text,
+    number text,
+    date date     
+);
+
+create table drivers
+(
+	id serial primary key,
+    fullname text,
+    birthday date,
+    category text     
+);
+
+create table car_drivers
+(
+	id serial primary key,
+    car_id int references cars(id),
+    driver_id int references drivers(id)    
+);
+
+
+alter table cars
+    add column color_id integer references colors(id),
+    add column pts_id integer references pts(id) unique;
+	
+	
